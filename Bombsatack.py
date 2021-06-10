776f07fde20340024cb9812457c2729eec2979c4
Stephen musk <78314574+bmurder@users.noreply.github.com>
2021-06-10 01:02:26 -0300

    Bombs atack

---
+++ Bombsatack.py
@@ -0,0 +1,257 @@
+import os 
+import time
+os.system('clear')
+
+print('--------------------------------')
+password = input("senha: ")
+print('--------------------------------')
+if (password== 'JJ289'):
+  print('acesso aceito')
+  time.sleep(3)
+  os.system('clear')
+  print('''
+  --------------------------------
+  arquivos:
+  
+  tsar 
+  Iron dome
+  Mark 14
+  Mark 16
+  B53/MK-53
+  Mk-36
+  H-Bomb
+  MK-24
+  Mk-17
+  TX-21
+  B41
+  --------------------------------
+  ''')
+  escolha = input('qual deseja utilizar? ')
+  if (escolha== 'tsar'):
+    print('''
+    regiões:
+    South America
+    north america
+    africa
+    europe
+    asia 
+    australia
+    ''')
+    pais1 = input('lançar em: ')
+    if (pais1== 'South America'):
+      print('lauching...')
+    elif (pais1== 'north America'):
+      print('lauching...')
+    elif (pais1== 'africa'):
+      print('lauching...')
+    elif (pais1== 'europe'):
+      print('lauching...')
+    elif (pais1== 'asia'):
+      print('lauching...')
+    elif (pais1== 'europe'):
+      print('lauching...')
+  elif (escolha== 'Iron dome'):
+    print('''
+    regiões:
+    South America
+    north america
+    africa
+    europe
+    asia 
+    australia
+    ''')
+    pais2 = input('lançar em: ')
+    if (pais2== 'South America'):
+      print('lauching...')
+    elif (pais2== 'north America'):
+      print('lauching...')
+    elif (pais2== 'africa'):
+      print('lauching...')
+    elif (pais2== 'europe'):
+      print('lauching...')
+    elif (pais2== 'asia'):
+      print('lauching...')
+    elif (pais2== 'europe'):
+      print('lauching...')
+  elif (escolha== 'Mark 14'):
+    print('''
+    regiões:
+    South America
+    north america
+    africa
+    europe
+    asia 
+    australia
+    ''')
+    pais3 = input('lançar em: ')
+    if (pais3== 'South America'):
+      print('lauching...')
+    elif (pais3== 'north America'):
+      print('lauching...')
+    elif (pais3== 'africa'):
+      print('lauching...')
+    elif (pais3== 'europe'):
+      print('lauching...')
+    elif (pais3== 'asia'):
+      print('lauching...')
+    elif (pais3== 'europe'):
+      print('lauching...')
+  elif (escolha== 'Mark 16'):
+    print('''
+    regiões:
+    South America
+    north america
+    africa
+    europe
+    asia 
+    australia
+    ''')
+    pais4 = input('lançar em: ')
+    if (pais4== 'South America'):
+      print('lauching...')
+    elif (pais4== 'north America'):
+      print('lauching...')
+    elif (pais4== 'africa'):
+      print('lauching...')
+    elif (pais4== 'europe'):
+      print('lauching...')
+    elif (pais4== 'asia'):
+      print('lauching...')
+    elif (pais4== 'europe'):
+      print('lauching...')
+  elif (escolha== 'B53/MK-53'):
+    print('''
+    regiões:
+    South America
+    north america
+    africa
+    europe
+    asia 
+    australia
+    ''')
+    pais5 = input('lançar em: ')
+    if (pais5== 'South America'):
+      print('lauching...')
+    elif (pais5== 'north America'):
+      print('lauching...')
+    elif (pais5== 'africa'):
+      print('lauching...')
+    elif (pais5== 'europe'):
+      print('lauching...')
+    elif (pais5== 'asia'):
+      print('lauching...')
+    elif (pais5== 'europe'):
+      print('lauching...')
+  elif (escolha== 'Mk-36'):
+    print('''
+    regiões:
+    South America
+    north america
+    africa
+    europe
+    asia 
+    australia
+    ''')
+    pais6 = input('lançar em: ')
+    if (pais6== 'South America'):
+      print('lauching...')
+    elif (pais6== 'north America'):
+      print('lauching...')
+    elif (pais6== 'africa'):
+      print('lauching...')
+    elif (pais6== 'europe'):
+      print('lauching...')
+    elif (pais6== 'asia'):
+      print('lauching...')
+    elif (pais6== 'europe'):
+      print('lauching...')
+  elif (escolha== 'H-Bomb'):
+    print('''
+    regiões:
+    South America
+    north america
+    africa
+    europe
+    asia 
+    australia
+    ''')
+    pais7 = input('lançar em: ')
+    if (pais7== 'South America'):
+      print('lauching...')
+    elif (pais7== 'north America'):
+      print('lauching...')
+    elif (pais7== 'africa'):
+      print('lauching...')
+    elif (pais7== 'europe'):
+      print('lauching...')
+    elif (pais7== 'asia'):
+      print('lauching...')
+    elif (pais7== 'europe'):
+      print('lauching...')
+  elif (escolha== 'MK-24'):
+    print('''
+    regiões:
+    South America
+    north america
+    africa
+    europe
+    asia 
+    australia
+    ''')
+    pais8 = input('lançar em: ')
+    if (pais8== 'South America'):
+      print('lauching...')
+    elif (pais8== 'north America'):
+      print('lauching...')
+    elif (pais8== 'africa'):
+      print('lauching...')
+    elif (pais8== 'europe'):
+      print('lauching...')
+    elif (pais8== 'asia'):
+      print('lauching...')
+    elif (pais8== 'europe'):
+      print('lauching...')
+  elif (escolha== 'Mk-17'):
+    print('''
+    regiões:
+    South America
+    north america
+    africa
+    europe
+    asia 
+    australia
+    ''')
+    pais9 = input('lançar em: ')
+    if (pais9== 'South America'):
+      print('lauching...')
+    elif (pais9== 'north America'):
+      print('lauching...')
+    elif (pais9== 'africa'):
+      print('lauching...')
+    elif (pais9== 'asia'):
+      print('lauching...')
+    elif (pais9== 'europe'):
+      print('lauching...')
+  elif (escolha== 'B41'):
+    print('''
+    regiões:
+    South America
+    north america
+    africa
+    europe
+    asia 
+    australia
+    ''')
+    pais10 = input('lançar em: ')
+    if (pais10== 'South America'):
+      print('lauching...')
+    elif (pais10== 'north America'):
+      print('lauching...')
+    elif (pais10== 'africa'):
+      print('lauching...')
+    elif (pais10== 'europe'):
+      print('lauching...')
+    elif (pais10== 'asia'):
+      print('lauching...')
+    elif (pais10== 'europe'):
+      print('lauching...')
